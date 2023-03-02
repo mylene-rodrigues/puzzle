08483f2b3d10134a417fe1041259f8f6c532ca19
mylene-rodrigues <115147744+mylene-rodrigues@users.noreply.github.com>
2023-03-02 11:19:59 +0100

    Slide Puzzle presque finis

---
+++ Puzzle/puzzle2.py
@@ -0,0 +1,157 @@
+import pygame
+import random
+import time
+from sprite import *
+from settings import *
+
+class Game():
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption(title)
+        self.clock = pygame.time.Clock()
+        self.shuffle_time = 0
+        self.start_shuffle = False
+        self.previous_choice = ""
+
+    def create_game(self):
+        grid = [[x + y * GAME_SIZE for x in range(1, GAME_SIZE + 1)] for y in range(GAME_SIZE)]
+        grid[-1][-1] = 0
+        return grid
+
+    def shuffle(self):
+        possible_moves = []
+        for row, tiles in enumerate(self.tiles):
+            for col, tile in enumerate(tiles):
+                if tile.text == "empty":
+                    if tile.right():
+                        possible_moves.append("right")
+                    if tile.left():
+                        possible_moves.append("left")
+                    if tile.up():
+                        possible_moves.append("up")
+                    if tile.down():
+                        possible_moves.append("down")
+                    break
+            if len(possible_moves) > 0:
+                break
+
+        if self.previous_choice == "right":
+            possible_moves.remove("left") if "left" in possible_moves else possible_moves
+        elif self.previous_choice == "left":
+            possible_moves.remove("right") if "right" in possible_moves else possible_moves
+        elif self.previous_choice == "up":
+            possible_moves.remove("down") if "down" in possible_moves else possible_moves
+        elif self.previous_choice == "down":
+            possible_moves.remove("up") if "up" in possible_moves else possible_moves
+
+            #import random
+        choice = random.choice(possible_moves)
+        self.previous_choice = choice
+        if choice == "right":
+            self.tiles_grid[row][col], self.tiles_grid[row][col + 1] = self.tiles_grid[row][col + 1], \
+                                                                       self.tiles_grid[row][col]
+        elif choice == "left":
+            self.tiles_grid[row][col], self.tiles_grid[row][col] = self.tiles_grid[row][col - 1], \
+                                                                   self.tiles_grid[row][col]
+        elif choice == "up":
+            self.tiles_grid[row][col], self.tiles_grid[row][col] = self.tiles_grid[row - 1][col], \
+                                                                   self.tiles_grid[row][col]
+        elif choice == "down":
+            self.tiles_grid[row][col], self.tiles_grid[row][col] = self.tiles_grid[row + 1][col], \
+                                                                   self.tiles_grid[row][col]
+
+    def draw_tiles(self):
+        self.tiles = []
+        for row, x in enumerate(self.tiles_grid):
+            self.tiles.append([])
+            for col, tile in enumerate(x):
+                if tile != 0:
+                    self.tiles[row].append(Tile(self, col, row, str(tile)))
+                else:
+                    self.tiles[row].append(Tile(self, col, row, "empty"))
+
+    def new(self):
+        self.all_sprites = pygame.sprite.Group()
+        self.tiles_grid = self.create_game()
+        self.tiles_grid_completed = self.create_game()
+        self.button_list = []
+        self.button_list.append(Button(775, 100, 200, 50, "Mélanger", WHITE, BLACK))
+        self.button_list.append(Button(775, 170, 200, 50, "Recommencer", WHITE, BLACK))
+        self.draw_tiles()
+
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw_tiles()
+
+    def update(self):
+        self.all_sprites.update()
+
+        if self.start_shuffle:
+            self.shuffle()
+            self.draw_tiles()
+            self.shuffle_time += 1
+            if self.shuffle_time > 100:
+                self.start_shuffle = False
+
+
+    def draw_grid(self): #grid pr le puzzle
+        for row in range(-1, GAME_SIZE * TILESIZE, TILESIZE):
+            pygame.draw.line(self.screen, LIGHTGREY, (row, 0), (row, GAME_SIZE * TILESIZE))
+        for col in range(-1, GAME_SIZE * TILESIZE, TILESIZE):
+            pygame.draw.line(self.screen, LIGHTGREY, (0, col), (GAME_SIZE * TILESIZE, col))
+
+
+    def draw(self):
+        self.screen.fill(BGCOLOUR)
+        self.all_sprites.draw(self.screen)
+        self.draw_grid()
+        for button in self.button_list:
+            button.draw(self.screen)
+        pygame.display.flip()
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit(0)
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                for row, tiles in enumerate(self.tiles):
+                    for col, tile in enumerate(tiles):
+                        if tile.click(mouse_x, mouse_y):
+                            if tile.right() and self.tiles_grid[row][col + 1] == 0:
+                                self.tiles_grid[row][col], self.tiles_grid[row][col + 1] = self.tiles_grid[row][col + 1], self.tiles_grid[row][col]
+
+                            if tile.left() and self.tiles_grid[row][col - 1] == 0:
+                                self.tiles_grid[row][col], self.tiles_grid[row][col - 1] = self.tiles_grid[row][col - 1], self.tiles_grid[row][col]
+
+                            if tile.up() and self.tiles_grid[row - 1][col] == 0:
+                                self.tiles_grid[row][col], self.tiles_grid[row - 1][col] = self.tiles_grid[row - 1][col], self.tiles_grid[row][col]
+
+                            if tile.down() and self.tiles_grid[row + 1][col] == 0:
+                                self.tiles_grid[row][col], self.tiles_grid[row + 1][col] = self.tiles_grid[row + 1][col], self.tiles_grid[row][col]
+
+
+                            self.draw_tiles()
+
+                for button in self.button_list:
+                    if button.click(mouse_x, mouse_y):
+                        if button.text == "shuffle":
+                            self.shuffl_time = 0
+                            self.start_shuffle = True
+                        if button.text == "restart":
+                            self.new()
+
+
+
+
+game = Game()
+while True:
+    game.new()
+    game.run()
---
+++ Puzzle/settings.py
@@ -0,0 +1,14 @@
+'#COLOR ( r, g, b)'
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+BGCOLOUR = DARKGREY
+
+'#GAME SETTINGS'
+WIDTH = 1100
+HEIGHT = 641
+FPS = 60
+title = "Sliding Puzzle"
+TILESIZE = 128
+GAME_SIZE = 3 #nbre de ligne et colonne(3x3)
